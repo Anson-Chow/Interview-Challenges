678c79770cf8d6b56d8ffe5d662c902becf14c73
Anson <chow.anson25@gmail.com>
2022-10-22 01:16:25 -0400

    ArrayChunk

--- questions/arrayChunk/index.js
+++ questions/arrayChunk/index.js
@@ -1,5 +1,5 @@
-/** Given an array along with a chunk size, return a new array that contains 
- * many subarrays where the length of each subarray is length `size`. 
+/** Given an array along with a chunk size, return a new array that contains
+ * many subarrays where the length of each subarray is length `size`.
  *
  * @example
  * arrayChunk([0, 1, 2, 3], 2) === [[0, 1], [2, 3]]
@@ -8,7 +8,33 @@
  */
 
 const arrayChunk = (array, size) => {
-  
+  // BEST PRACTISE
+  // Create an empty 'chunkedArray' to hold the subarrays
+  const chunkedArray = [];
+
+  // For each index in the array that is divisible by 'size'
+  for (let i = 0; i < array.length; i += size) {
+    // Create a subarray of length 'size' that slices from the current index
+    const subarray = array.slice(i, i + size);
+    // Push this subarray onto our 'chunkedArray'
+    chunkedArray.push(subarray);
+  }
+
+  return chunkedArray;
 };
+
+// SIMPLE TO UNDERSTAND
+//   const arrayChunk2 = (array, size) => {
+//     const chunkedArray = [];
+
+//     for (let i = 0; i < array.length; ++i) {
+//       if (i % size !== 0) {
+//         continue;
+//       }
+//       const subarray = array.slice(i, i + size);
+//       chunkedArray.push(subarray);
+//     }
+
+//     return chunkedArray;
 
 module.exports = arrayChunk;
