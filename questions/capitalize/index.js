d6314550684b9ea26b5b0a21437014738bc9f685
Anson <chow.anson25@gmail.com>
2022-10-22 13:10:59 -0400

    CapitalizeSentence and Steps

--- questions/capitalize/index.js
+++ questions/capitalize/index.js
@@ -1,4 +1,4 @@
-/** Given a sentence, capitalize the first letter of each word and 
+/** Given a sentence, capitalize the first letter of each word and
  * return the capitalized sentence.
  *
  * @example
@@ -8,7 +8,27 @@
  */
 
 const capitalize = (str) => {
-  
+  const result = [];
+  for (let word of str.split(" ")) {
+    const capitalizedWord = word[0].toUpperCase() + word.slice(1);
+    result.push(capitalizedWord);
+  }
+  return result.join(" ");
 };
+
+// const capitalize2 = (str) => {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; ++i) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     }
+//     else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// };
 
 module.exports = capitalize;
